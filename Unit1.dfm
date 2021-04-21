61088177210b639551cd668b109ad8669c771a28
Tayberline <7dimakakdela7@gmail.com>
2021-04-21 11:53:40 +0700

    сделанное 3.1

--- Unit1.dfm
+++ Unit1.dfm
@@ -11,6 +11,25 @@ object Form1: TForm1
   Font.Name = 'Tahoma'
   Font.Style = []
   OldCreateOrder = False
+  OnCreate = FormCreate
+  OnMouseMove = FormMouseMove
+  OnMouseUp = FormMouseUp
   PixelsPerInch = 96
   TextHeight = 13
+  object SpeedButton1: TSpeedButton
+    Left = 128
+    Top = 200
+    Width = 23
+    Height = 22
+    OnClick = SpeedButton1Click
+  end
+  object Panel1: TPanel
+    Left = 32
+    Top = 40
+    Width = 185
+    Height = 41
+    Caption = 'Panel1'
+    TabOrder = 0
+    OnMouseDown = Panel1MouseDown
+  end
 end
--- Unit1.pas
+++ Unit1.pas
@@ -4,10 +4,19 @@ interface
 
 uses
   Winapi.Windows, Winapi.Messages, System.SysUtils, System.Variants, System.Classes, Vcl.Graphics,
-  Vcl.Controls, Vcl.Forms, Vcl.Dialogs;
+  Vcl.Controls, Vcl.Forms, Vcl.Dialogs, Vcl.ExtCtrls, Vcl.Buttons;
 
 type
   TForm1 = class(TForm)
+    Panel1: TPanel;
+    SpeedButton1: TSpeedButton;
+    procedure FormCreate(Sender: TObject);
+    procedure Panel1MouseDown(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure FormMouseMove(Sender: TObject; Shift: TShiftState; X, Y: Integer);
+    procedure FormMouseUp(Sender: TObject; Button: TMouseButton;
+      Shift: TShiftState; X, Y: Integer);
+    procedure SpeedButton1Click(Sender: TObject);
   private
     { Private declarations }
   public
@@ -15,10 +24,49 @@ type
   end;
 
 var
-  Form1: TForm1;
+  Form1: TForm1; Xset, Yset: integer; Drug:Boolean;
 
 implementation
 
 {$R *.dfm}
+
+procedure TForm1.FormCreate(Sender: TObject);
+begin
+ Drug:=False;
+end;
+
+procedure TForm1.FormMouseMove(Sender: TObject; Shift: TShiftState; X,
+  Y: Integer);
+begin
+ if Drug=true then
+  begin
+    Panel1.Left:=X-Xset;
+    Panel1.Top:=Y-Yset;
+  end;
+end;
+
+procedure TForm1.FormMouseUp(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+Drug:=False;
+ReleaseCapture;
+end;
+
+procedure TForm1.Panel1MouseDown(Sender: TObject; Button: TMouseButton;
+  Shift: TShiftState; X, Y: Integer);
+begin
+ Drug:=true;
+ Xset:=X;
+ Yset:=y;
+ SetCapture(Form1.Handle);
+
+end;
+
+procedure TForm1.SpeedButton1Click(Sender: TObject);
+begin
+ SpeedButton1.Parent:=Panel1;
+ SpeedButton1.Left:=0;
+ SpeedButton1.top:=0;
+end;
 
 end.
